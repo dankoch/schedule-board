6f943a7b978961b7701456782eb486aebeeb39d8
Daniel Koch <dkoch@Daniels-MacBook-Pro.local>
2018-09-09 18:59:23 -0700

    More non-functional commits – just to back things up.

--- src/central_cinema_schedule_client.cpp
+++ src/central_cinema_schedule_client.cpp
@@ -3,7 +3,9 @@
 #include <vector>
 #include <string>
 #include <ctime>
+#include <time.h>
 #include <curl/curl.h>
+#include <algorithm>
 
 #include "spdlog/spdlog.h"
 #include "gumbo.h"
@@ -11,12 +13,17 @@
 #include "config_options.h"
 #include "calendar_event.h"
 #include "file_utils.h"
+#include "string_utils.h"
 
 #include "schedule_client.h"
 
 class CentralCinemaScheduleClient : ScheduleClient {
-  const std::string EVENT_NAME_CLASS = "event-title";
-  const std::string CLASS_ATTRIBUTE = "class";
+  const std::string TB_MONTH_EVENT_TABLE_CELL_CLASS = "futureday";
+  const std::string TB_DAYOFMONTH_CLASS = "dayofmonth-num";
+  const std::string TB_EVENTTITLE_CLASS = "event-title";
+  const std::string TB_EVENTTIME_CLASS = "event-time";
+
+  const std::string HTML_CLASS_ATTRIBUTE = "class";
 
   const std::string URL_TEMPLATE = "https://public.ticketbiscuit.com/CentralCinema/Calendar/%d/%d";
   const std::string FILE_SUBPATH = "/central";
@@ -28,10 +35,15 @@ class CentralCinemaScheduleClient : ScheduleClient {
 
   std::vector<CalendarEvent> loadedCalendarEvents;
 
-  struct MonthBucket {
+  struct MonthRequest {
     int month,
-    int year
+    int year,
+    std::string filePath
   };
+
+  struct FetchedDataMetadata {
+    
+  }
 
 public:
   CentralCinemaScheduleClient(std::shared_ptr<ConfigOptions> options) : _options(options) {
@@ -48,23 +60,23 @@ private:
     std::vector<CalendarEvent> fetchedEvents;
 
     /* Determine which buckets need to be queried. */
-    std::vector<MonthBucket> monthsToQuery;
+    std::vector<MonthRequest> monthsToQuery;
 
     for (int yearIndex = startDate->tm_year; yearIndex <= endDate->tm_year; yearIndex++) {
       if (yearIndex != startDate->tm_year && yearIndex != endDate->tm_year) {
         /* if current year doesn't match the start or end year, we need to query the entire year. */
         for (int monthIndex = 0; monthIndex < 12; monthIndex++) {
-          monthsToQuery.push_back(MonthBucket(monthIndex, yearIndex));
+          monthsToQuery.push_back(MonthRequest(monthIndex, yearIndex, ""));
         }
       } else if (yearIndex != endDate->tm_month) {
         /* the startDate year isn't this year, but the endDate is.  Add all the months from start of year until endDate. */
         for (int monthIndex = monthIndex->tm_month; monthIndex <= 11; monthIndex++) {
-          monthsToQuery.push_back(MonthBucket(monthIndex, yearIndex));
+          monthsToQuery.push_back(MonthRequest(monthIndex, yearIndex, ""));
         }
       } else if (yearIndex != startDate->tm_year) {
         /* the endDate year isn't this year, but the startDate is.  Add all the months through EOY. */
         for (int monthIndex = 0; monthIndex <= endDate->tm_month; monthIndex++) {
-          monthsToQuery.push_back(MonthBucket(monthIndex, yearIndex));
+          monthsToQuery.push_back(MonthRequest(monthIndex, yearIndex, ""));
         }
       }
     }
@@ -74,25 +86,27 @@ private:
     util::clearDirectory(downloadDir);
 
     /* Download the collector data for each bucket */
-    std::vector<std::string> extractPaths;
+    std::vector<MonthRequest> downloadedMonths;
     for(auto const bucket : monthsToQuery) {
+      MonthRequest monthExtract;
 
-      std::string extractPath;
-      if (downloadTicketBiscuitCalendarExtract(downloadDir, bucket, &extractPath) == 0) {
-        extractPaths.push_back(extractPath);
+      if (downloadTicketBiscuitCalendarExtract(downloadDir, bucket, monthExtract) == 0 && !monthExtract.filePath.empty()) {
+        downloadedMonths.push_back(monthExtract);
       }
     }
 
     /* If we succesfully downloaded any files, extract the events from them and overwrite our data file.*/
-    if (extractPaths.size() > 0) {
-      for (auto const extractPath : extractPaths) {
-
+    if (downloadedMonths.size() > 0) {
+      for (auto const monthExtract : downloadedMonths) {
+        parseTicketBiscuitCalendarExtract(monthExtract, fetchedEvents);
       }
+
+      std::sort(fetchedEvents.begin(), fetchedEvents.end());
     }
   }
 
-  int downloadTicketBiscuitCalendarExtract(std::string dir, MonthBucket bucket, std::string& extractPath) {
-    int returnCode = 0;
+  int downloadTicketBiscuitCalendarExtract(std::string dir, MonthRequest bucket, MonthRequest* monthExtract) {
+    int returnCode = -1;
 
     /* Ensure the target directory for the download extract exists. */
     createStorageDir(dir);
@@ -125,7 +139,6 @@ private:
       _logger->info("Made request to: {} to write output to {}", requestURL, requestDestination);
 
       if(res != CURLE_OK) {
-        returnCode = 1;
         size_t len = strlen(errbuf);
 
         if(len)
@@ -133,7 +146,8 @@ private:
         else
           _logger->error("Failed making request to: {}, {}", requestURL, curl_easy_strerror(res));
       } else {
-        extractPath = requestDestination;
+        monthExtract = new MonthRequest(bucket.month, bucket.year, requestDestination);
+        returnCode = 0;
       }
 
       curl_easy_cleanup(curl_handle);
@@ -143,18 +157,7 @@ private:
     return returnCode;
   }
 
-  int parseTicketBiscuitCalendarExtract(std::string extractPath, std::vector<CalendarEvent>& fetchedEvents) {
-    int returnCode = 0;
-
-    GumboOutput* output = gumbo_parse(util::getFileContents(extractPath).str_c());
-    getEvents(output->root);
-
-    gumbo_destroy_output(&kGumboDefaultOptions, output);
-
-    return returnCode;
-  }  
-
-  std::string buildTicketBiscuitRequestURL(MonthBucket bucket) {
+  std::string buildTicketBiscuitRequestURL(MonthRequest bucket) {
     char buffer[75];
 
     sprintf(buffer, URL_TEMPLATE.c_str(), bucket.year, bucket.month);
@@ -164,37 +167,91 @@ private:
   std::string baseDirectory() {
     return _options->getStoragePath() + FILE_SUBPATH;
   }
+
+
+  int parseTicketBiscuitCalendarExtract(MonthRequest extract, std::vector<CalendarEvent>& fetchedEvents) {
+    int returnCode = 0;
+
+    GumboOutput* output = gumbo_parse(util::getFileContents(extract.filePath).str_c());
+    traverseTicketBiscuiltCalendarHtmlTree(output->root, extract, fetchedEvents);
+
+    gumbo_destroy_output(&kGumboDefaultOptions, output);
+
+    return returnCode;
+  }
+
+  void traverseTicketBiscuitCalendarHtmlTree(GumboNode* node, const MonthRequest& extract, 
+                                                            std::vector<CalendarEvent>& fetchedEvents) {
+
+    /* We've reached a terminus.  Break the recursion. */
+    if (node->type != GUMBO_NODE_ELEMENT) {
+      return;
+    }
+
+    /* Check to see if this is a 'futureday' table cell.  If so, parse the cell as a discrete event. */
+    GumboAttribute* classList;
+    if ((classList = gumbo_get_attribute(&node->v.element.attributes, HTML_CLASS_ATTRIBUTE))) {
+      if (strcmp(classList->value, TB_MONTH_EVENT_TABLE_CELL_CLASS) == 0) {
+        CalendarEvent eventEntry;
+        eventEntry.setYear(extract.year);
+        eventEntry.setMonth(extract.month);
+
+        traverseTicketBiscuitEventCell(node, eventEntry);
+        fetchedEvents.push_back(eventEntry);        
+      }
+    }
+
+    /* Otherwise, keep recursing. */
+    GumboVector* children = &node->v.element.children;
+    for (unsigned int i = 0; i < children->length; ++i) {
+      traverseTicketBiscuitCalendarHtmlTree(static_cast<GumboNode*>(children->data[i]));
+    }
+  };
+
+  void traverseTicketBiscuitEventCell(GumboNode* node, CalendarEvent& eventEntry) {
+
+    /* Check to see if this node has any of the HTML classes that indicate it contains a key schedule text. */
+    GumboAttribute* classList;
+    if ((classList = gumbo_get_attribute(&node->v.element.attributes, HTML_CLASS_ATTRIBUTE))) {
+      if (strcmp(classList->value, TB_DAYOFMONTH_CLASS) == 0) {
+        eventEntry.setDayOfMonth(std::stoi(getNodeText(node)));       
+      } else if (strcmp(classList->value, TB_EVENTTITLE_CLASS) == 0) {
+        eventEntry.setName(getNodeText(node));         
+      } else if (strcmp(classList->value, TB_EVENTTIME_CLASS) == 0) {
+        std::tm timeOfDay;
+        strptime(getNodeText(node).c_str(), "%I:%M %p", &timeOfDay);
+
+        eventEntry.setHour(timeOfDay.tm_hour);
+        eventEntry.setMinute(timeOfDay.tm_min);      
+      }
+    }
+
+    /* Otherwise, keep recursing. */
+    GumboVector* children = &node->v.element.children;
+    for (unsigned int i = 0; i < children->length; ++i) {
+      traverseTicketBiscuitEventCell(static_cast<GumboNode*>(children->data[i]));
+    }    
+  }
 };
 
-static const void getText(GumboNode *node) {
+static std::string getNodeText(GumboNode *node) {
   if (node->type == GUMBO_NODE_TEXT) {
-    std::cout << node->v.text.text << std::endl;  
+    return std::string(node->v.text.text);
   }
 
   if (node->type == GUMBO_NODE_ELEMENT) {
     GumboVector* children = &node->v.element.children;
     for (unsigned int i = 0; i < children->length; ++i) {
-      getText(static_cast<GumboNode*>(children->data[i]));
+      std::string nodeText = getNodeText(static_cast<GumboNode*>(children->data[i]));
+
+      if (!nodeText.empty()) {
+        return util::trim(nodeText);
+      }
     }
   }
+
+  return "";
 }
-
-static void getEvents(GumboNode* node) {
-  if (node->type != GUMBO_NODE_ELEMENT) {
-    return;
-  }
-  GumboAttribute* classList;
-  if ((classList = gumbo_get_attribute(&node->v.element.attributes, CLASS_ATTRIBUTE))) {
-      if (strcmp(classList->value, EVENT_NAME_CLASS) == 0) {
-        getText(node);        
-      }
-  }
-
-  GumboVector* children = &node->v.element.children;
-  for (unsigned int i = 0; i < children->length; ++i) {
-    getEvents(static_cast<GumboNode*>(children->data[i]));
-  }
-};
 
 int main(int argc, char** argv) {
   std::shared_ptr<ConfigOptions> options(new ConfigOptions(argc, argv));
---
+++ src/util/string_utils.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+#include "string_utils.h"
+
+namespace util {
+  int trim(std::string stringToTrim) {
+    return rmdir(path.str_c())
+	}
+}
